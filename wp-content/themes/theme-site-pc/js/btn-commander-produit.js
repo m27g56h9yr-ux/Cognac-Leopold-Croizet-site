291769c5371860518445707fd1b09df3aa866b18
Léo <m27g56h9yr@privaterelay.appleid.com>
2026-06-09 22:21:43 +0200

    Link Russian product order buttons to AV

--- wp-content/themes/theme-site-pc/js/btn-commander-produit.js
+++ wp-content/themes/theme-site-pc/js/btn-commander-produit.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    repair_russian_vsop_order_link();
+    repair_russian_order_link();
 
     timer = setTimeout(function () {
         if (!$(".flex-control-thumbs").length) {
@@ -12,9 +12,25 @@ $(document).ready(function () {
         }
     }, 50);
 
-    function repair_russian_vsop_order_link() {
-        if (window.location.pathname.indexOf("/ru/collection/vsop/") === -1) return;
-        $(".btn-commander-produit").attr("href", "https://av.ru/i/174054");
+    function repair_russian_order_link() {
+        var orderLinks = {
+            "/ru/collection/vs/": "https://av.ru/i/1021709",
+            "/ru/collection/vsop/": "https://av.ru/i/174054",
+            "/ru/collection/napoleon/": "https://av.ru/i/1020490",
+            "/ru/collection/xo/": "https://av.ru/i/1020491",
+            "/ru/collection/xo-exception/": "https://av.ru/i/1005624",
+            "/ru/collection/extra/": "https://av.ru/i/174057",
+            "/ru/collection/excellence/": "https://av.ru/i/231809",
+            "/ru/collection/heritage/": "https://av.ru/search/?freeText=Leopold%20Croizet%20Heritage",
+            "/ru/collection/valentine/": "https://av.ru/i/178511",
+        };
+        var pathname = window.location.pathname.replace(/\/?$/, "/");
+        var orderLink = Object.keys(orderLinks).reduce(function (link, route) {
+            return link || (pathname.indexOf(route) !== -1 ? orderLinks[route] : "");
+        }, "");
+        if (!orderLink) return;
+
+        $(".btn-commander-produit").attr("href", orderLink);
     }
 
 });
