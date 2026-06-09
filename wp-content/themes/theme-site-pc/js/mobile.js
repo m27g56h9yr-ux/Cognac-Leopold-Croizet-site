1ae6e453cfbabefed9e68532298d2bd4849cb3dd
Léo <m27g56h9yr@privaterelay.appleid.com>
2026-06-09 22:25:37 +0200

    Add Russian AV order links fallback

--- wp-content/themes/theme-site-pc/js/mobile.js
+++ wp-content/themes/theme-site-pc/js/mobile.js
@@ -69,8 +69,24 @@ $(document).ready(function () {
     }
 
     function repair_russian_order_links() {
-        if (!window.location.pathname.match(/\/ru\/collection\/vsop\/?$/)) return;
-        $(".btn-commander-produit").attr("href", "https://av.ru/i/174054");
+        var orderLinks = {
+            "/ru/collection/vs/": "https://av.ru/i/1021709",
+            "/ru/collection/vsop/": "https://av.ru/i/174054",
+            "/ru/collection/napoleon/": "https://av.ru/i/1020490",
+            "/ru/collection/xo/": "https://av.ru/i/1020491",
+            "/ru/collection/xo-exception/": "https://av.ru/i/1005624",
+            "/ru/collection/extra/": "https://av.ru/i/174057",
+            "/ru/collection/excellence/": "https://av.ru/i/231809",
+            "/ru/collection/heritage/": "https://av.ru/search/?freeText=Leopold%20Croizet%20Heritage",
+            "/ru/collection/valentine/": "https://av.ru/i/178511",
+        };
+        var pathname = window.location.pathname.replace(/\/?$/, "/");
+        var orderLink = Object.keys(orderLinks).reduce(function (link, route) {
+            return link || (pathname.indexOf(route) !== -1 ? orderLinks[route] : "");
+        }, "");
+        if (!orderLink) return;
+
+        $(".btn-commander-produit").attr("href", orderLink);
     }
 
     $(".navbar-toggler").on("click", function () {
